22ae2b866c9f4e0ad61282b78dd504be25864bef
Ning Yang <yangning10000@gmail.com>
2019-03-30 15:06:45 -0700

    fix

--- dist/vue-apollo.umd.js
+++ dist/vue-apollo.umd.js
@@ -1897,49 +1897,11 @@
 
       for (var key in apollo) {
         if (key.charAt(0) !== '$') {
-          var options = apollo[key]; // if (this.$isServer) {
-          //   let prefetchID = this.$store.state.userMeta.userMeta.prefetchID;
-          //   if (options.prefetch !== false && apollo.$prefetch !== false && options.prefetch !== undefined) {
-          //     if (!prefetchIDs[prefetchID]) {
-          //       prefetchIDs[prefetchID] = {time: new Date().getTime(), value: []};
-          //     } 
-          //     for (let i of options.query.definitions[0].selectionSet.selections) {
-          //       let variables = {};
-          //       for (let j of i.arguments) {
-          //         let variableName = j.name.value;
-          //         if (j.value && j.value.kind === 'ObjectValue') {
-          //           for (let i of j.value.fields) {
-          //             if (variables[variableName] === undefined) {
-          //               variables[variableName] = {}
-          //             }
-          //             if (typeof options.variables === "function" && options.variables.call(this)[i.name.value] !== undefined) {
-          //               variables[variableName][i.name.value] = options.variables.call(this)[i.name.value];
-          //             } else {
-          //               if (i.value.value !== undefined) {
-          //                 variables[variableName][i.name.value] = i.value.value
-          //               }
-          //             }
-          //           }
-          //         } else {
-          //           if (typeof options.variables === "function" && options.variables.call(this)[variableName] !== undefined) {
-          //             variables[variableName] = options.variables.call(this)[variableName];
-          //           } else {
-          //             if (options.variables[variableName] !== undefined) {
-          //               variables[variableName] = options.variables[variableName]
-          //             }
-          //           }
-          //         }
-          //       }
-          //       prefetchIDs[prefetchID].value.push({ name: i.name.value, variables: variables });
-          //     }
-          //     options.fetchPolicy = 'cache-first';
-          //     this.$_apolloPromises.push(this.$apollo.addSmartQuery(key, options).firstRun);
-          //     options.fetchPolicy = 'network-only';
-          //     this.$apollo.addSmartQuery(key, options, true);
-          //   }
-          // } else {
-          //   this.$apollo.addSmartQuery(key, options)
-          // }
+          var options = apollo[key];
+
+          {
+            this.$apollo.addSmartQuery(key, options);
+          }
         }
       }
 
