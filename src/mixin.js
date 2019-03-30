6dc32a05981d387aee4b1ecc38815407319b7d4f
Ning Yang <yangning10000@gmail.com>
2019-03-30 16:03:25 -0700

    fix

--- src/mixin.js
+++ src/mixin.js
@@ -96,35 +96,36 @@ function launch () {
           if (options.prefetch !== false && apollo.$prefetch !== false && options.prefetch !== undefined) {
             if (!prefetchIDs[prefetchID]) {
               prefetchIDs[prefetchID] = {time: new Date().getTime(), value: []};
-            // for (let i of options.query.definitions[0].selectionSet.selections) {
-            //   let variables = {};
-            //   for (let j of i.arguments) {
-            //     let variableName = j.name.value;
-            //     if (j.value && j.value.kind === 'ObjectValue') {
-            //       for (let i of j.value.fields) {
-            //         if (variables[variableName] === undefined) {
-            //           variables[variableName] = {}
-            //         }
-            //         if (typeof options.variables === "function" && options.variables.call(this)[i.name.value] !== undefined) {
-            //           variables[variableName][i.name.value] = options.variables.call(this)[i.name.value];
-            //         } else {
-            //           if (i.value.value !== undefined) {
-            //             variables[variableName][i.name.value] = i.value.value
-            //           }
-            //         }
-            //       }
-            //     } else {
-            //       if (typeof options.variables === "function" && options.variables.call(this)[variableName] !== undefined) {
-            //         variables[variableName] = options.variables.call(this)[variableName];
-            //       } else {
-            //         if (options.variables[variableName] !== undefined) {
-            //           variables[variableName] = options.variables[variableName]
-            //         }
-            //       }
-            //     }
-            //   }
-            //   prefetchIDs[prefetchID].value.push({ name: i.name.value, variables: variables });
-            // }
+            }
+            for (let i of options.query.definitions[0].selectionSet.selections) {
+              let variables = {};
+              for (let j of i.arguments) {
+                let variableName = j.name.value;
+                if (j.value && j.value.kind === 'ObjectValue') {
+                  for (let i of j.value.fields) {
+                    if (variables[variableName] === undefined) {
+                      variables[variableName] = {}
+                    }
+                    if (typeof options.variables === "function" && options.variables.call(this)[i.name.value] !== undefined) {
+                      variables[variableName][i.name.value] = options.variables.call(this)[i.name.value];
+                    } else {
+                      if (i.value.value !== undefined) {
+                        variables[variableName][i.name.value] = i.value.value
+                      }
+                    }
+                  }
+                } else {
+                  if (typeof options.variables === "function" && options.variables.call(this)[variableName] !== undefined) {
+                    variables[variableName] = options.variables.call(this)[variableName];
+                  } else {
+                    if (options.variables[variableName] !== undefined) {
+                      variables[variableName] = options.variables[variableName]
+                    }
+                  }
+                }
+              }
+              prefetchIDs[prefetchID].value.push({ name: i.name.value, variables: variables });
+            }
             options.fetchPolicy = 'cache-first';
             this.$_apolloPromises.push(this.$apollo.addSmartQuery(key, options).firstRun);
             options.fetchPolicy = 'network-only';
