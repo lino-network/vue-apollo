6a79f2ba7b83de194da775c8c98c07541bc064ab
Ning Yang <yangning10000@gmail.com>
2019-05-21 14:43:26 -0700

    build

--- dist/vue-apollo.esm.js
+++ dist/vue-apollo.esm.js
@@ -1947,7 +1947,9 @@ function installMixin(Vue, vueVersion) {
     },
     serverPrefetch: function serverPrefetch() {
       if (this.$_apolloPromises) {
-        return Promise.all(this.$_apolloPromises);
+        return Promise.all(this.$_apolloPromises).catch(function (err) {
+          console.log('apollo prefetch error: ', err);
+        });
       }
     }
   } : {}, {
